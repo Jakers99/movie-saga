692dc3157ac1e30ecc1365ba13e7a5b269c3225d
Jake Dahl <jakedahl16@yahoo.com>
2020-06-19 16:43:10 -0500

    added junction table

--- database.sql
+++ database.sql
@@ -16,7 +16,12 @@ CREATE TABLE "genres" (
 -- You will need to create the junction table that stores the relationships between "movies" and "genres"
 -- This table will need to be populated with some data as well (INSERTS)
 -- Recall that this Junction Table will just be a table of ids!
-
+CREATE TABLE "movies_genres"
+(
+  "id" SERIAL PRIMARY KEY,
+  "movies_id" INT REFERENCES "movies",
+  "genres_id" INT REFERENCES "genres"
+);
 
 
 --------[ DATA! ]---------
